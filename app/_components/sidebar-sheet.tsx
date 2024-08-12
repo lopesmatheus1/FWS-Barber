a21d568f2007951ba28456c951034818d2370a71
Matheus Lopes Marendino <lopesmatheus1@hotmail.com>
2024-08-12 19:37:17 -0300

    feat: ensure bookings are listed on bookings page

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -75,9 +75,11 @@ const SidebarButton = () => {
           </Button>
         </SheetClose>
 
-        <Button variant="ghost" className="justify-start gap-2">
-          <CalendarIcon size={18} />
-          Agendamentos
+        <Button variant="ghost" className="justify-start gap-2" asChild>
+          <Link href="/bookings">
+            <CalendarIcon size={18} />
+            Agendamentos
+          </Link>
         </Button>
       </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,38 @@ import BarbershopItem from "./_components/barbershop-item"
 import { QuickSearchOptions } from "./_constants/search"
 import Footer from "./_components/footer"
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
 
 const Home = async () => {
+  const session = await getServerSession(authOptions)
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
+
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session?.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
 
   return (
     <div>
@@ -67,7 +91,14 @@ const Home = async () => {
         </div>
 
         {/*AGENDAMENTO*/}
-        <BookingItem></BookingItem>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
